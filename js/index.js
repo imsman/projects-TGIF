0746bb0a481106d525de6ffcfba8a126bf0ed366
an-saemi <106298277+an-saemi@users.noreply.github.com>
2022-06-24 10:49:17 +0900

    0624
    태그바꾸기 및 json파일

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$('.bannerAll').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+});
+//마우스올리면 잠시 일시정지하는거
+
+// $('.sitemap').on('click',function(){
+//     $(this).addClass('active');
+
+//     $(document).on('click',function(){
+//         console.log(classList);
+//     })
+// });
