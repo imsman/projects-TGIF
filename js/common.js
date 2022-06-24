ac1c558aa6a534aa2a4aa64b19dab2844c470942
an-saemi <106298277+an-saemi@users.noreply.github.com>
2022-06-24 11:19:25 +0900

    헤더,헬퍼,푸터 고치기

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
-$('.helper').load('inc.html > .helper > a')
+$('.helper').load('../inc.html > .helper > div')
 $('header').load('../inc.html > header > .navi');
 $('footer').load('../inc.html > footer > .footer_bg');
+
+
+
+$('.helper').on('click',function(){
+    $('.helper .sitemap').toggleClass('active');
+})
