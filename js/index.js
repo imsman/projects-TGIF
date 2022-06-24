fbfe373f81295d29733441c406f28097e7a8c98f
an-saemi <106298277+an-saemi@users.noreply.github.com>
2022-06-24 13:05:32 +0900

    index pause on hover

--- js/index.js
+++ js/index.js
@@ -3,13 +3,7 @@ $('.bannerAll').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
+    pauseOnHover:true
 });
 //마우스올리면 잠시 일시정지하는거
 
-// $('.sitemap').on('click',function(){
-//     $(this).addClass('active');
-
-//     $(document).on('click',function(){
-//         console.log(classList);
-//     })
-// });
