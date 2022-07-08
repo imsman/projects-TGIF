ebd5492729dded83f1afa0fa12d8a1959262c655
an-saemi <ryogm97@gmail.com>
2022-07-08 12:07:39 +0900

    appe json하고 ajax 스크립트

--- js/common.js
+++ js/common.js
@@ -1,9 +1,19 @@
-$('.helper').load('inc.html > .helper > div')
+$('.helper').load('inc.html > .helper > .all', navi);
 $('header').load('inc.html > header > .navi');
 $('footer').load('inc.html > footer > .footer_bg');
 
 
 
-$('.helper').on('click',function(){
-    $('.helper .sitemap').toggleClass('active');
-})
+
+function navi() {
+    const help = document.querySelector('.helper .helpin > a:first-child');
+    const sitemap = document.querySelector('.sitemap');
+
+    help.addEventListener('click', function () {
+        sitemap.classList.add('active');
+        
+        sitemap.addEventListener('click',function(){
+            this.classList.remove('active');
+        })
+    })
+}
